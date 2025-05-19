f1bf8f48f5841531af9e2f6f440783c05f31b10a
Suvansh Choudhary <choudharysuvansh04@gmail.com>
2025-05-20 00:43:16 +0530

    Final commit all checks done

--- backend/app/main.py
+++ backend/app/main.py
@@ -4,14 +4,14 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Product Autocomplete API")
 
-# ✅ Add CORS to the *same* app instance
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # or ["*"] for dev
+    allow_origins=["http://localhost:5173"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# ✅ Include your router
+
 app.include_router(router)
